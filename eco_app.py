3f82046aa3e1cf019abb3e92903be2e14f5e6a15
fredd <freddypcarrion@gmail.com>
2022-08-10 11:01:44 -0500

    Design of Front-end

--- eco_app.py
+++ eco_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Title
-st.title("**Economic Assessment App**")
+st.title("**Tuma App**")
 st.write("This app is about...")
 
 # Create menu of options
@@ -15,3 +15,28 @@ with st.sidebar:
         icons=["house", "clipboard-data", "tv", "calculator"],
     )
 
+# Economic inputs
+if options == "Economic Inputs":
+    discount_rate = st.number_input("Enter the discount rate: ")
+    inflation = st.number_input("Enter the inflation: ")
+    oil_price = st.number_input("Enter the oil price: ")
+    taxes = st.number_input("Enter the taxes: ")
+    capex = st.number_input("Enter the initial investment")
+
+elif options == "Technical Inputs":
+    time = st.number_input('Enter the time for the project in years:')
+    wells_type = st.selectbox("Choose the well type", ('Horizontal', 'Directional', 'Vertical'))
+    if wells_type == "Horizontal":
+        q = st.number_input('Enter the flow rate for a horizontal well: ')
+    elif wells_type == "Directional":
+        q = st.number_input('Enter the flow rate for a directional well: ')
+    elif wells_type == "Vertical":
+        q = st.number_input('Enter the flow rate for a vertical well: ')
+
+    wo_type = st.selectbox("Choose the workover type", ('Upsizing', 'Acid Stimulation', 'Hydraulic fracturing'))
+    if wo_type == "Upsizing":
+        dq = st.number_input('Enter the enhanced percentage of flow rate: ')
+    elif wo_type == "Acid Stimulation":
+        dq = st.number_input('Enter the enhanced percentage of flow rate: ')
+    elif wo_type == "Hydraulic fracturing":
+        dq = st.number_input('Enter the enhanced percentage of flow rate: ')
