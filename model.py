62fecf04ed0e663d7ba7a1a384972f46cab2153e
jorollig <jorollig@gmail.com>
2022-08-15 18:22:06 -0500

    Funciones del flujo de caja

--- model.py
+++ model.py
@@ -18,9 +18,20 @@ frac = df_cf['fracturing'].values
 #define inputs for qarps
 t = df_cf['año'].values
 
+depreciacion = (costo - rescate) / time
+
 # Oil production rate
 def qarps(t,qi,di,b = 0):
     return qi / (np.abs((1 + b * di * (t*365)))**(1/b))
+
+df_cf['produccion'] = qarps(t,qi,di,b)
+df_cf['ingresos'] = df_cf['produccion'] * df_cf['precio']
+df_cf['IO'] = df_cf['ingresos'] - df_cf['OPEX']
+df_cf['BTCF'] = df_cf['IO'] - df_cf['CAPEX']
+df_cf['IG'] = df_cf['IO'] - depreciacion
+df_cf['impuesto'] = df_cf['IG']*tasa_imp
+
+df_cf[ATCF] = df_cf['depreciación'] - df_cf['impuesto']
 
 
 
